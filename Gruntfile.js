e5022379b6d6424fc72f1aa2bf86254410d1316d
Shane Osbourne <shane.osbourne8@gmail.com>
2013-10-27 14:23:48 +0000

    Update gruntfile with new tasks & remove repetition

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,156 +1,201 @@
-module.exports = function( grunt ) {
-  grunt.initConfig({
-    pkg               : grunt.file.readJSON( 'package.json' ),
-    js_dir            : 'public/js',
-    js_deps           : '<%= js_dir %>/dist/deps',
-    js_amd            : '<%= js_dir %>/dist/modules',
-    js_all            : '<%= js_dir %>/dist/combined',
-    js_dist_file      : '<%= js_all %>.clean.min.js',
-    php_files         : {
-      layout : 'app/views/layouts/master.blade.php'
-    },
-    sass_dir          : 'public/sass',
-    css_dir           : 'public/css',
-    css_dist_file     : 'public/css/style.css',
-    concat            : {
-      deps  : {
-        files : [
-          {
-            src  : [
-              /** todo - Automate the insertion of these file paths **/
-              'public/components/jquery/jquery.min.js',
-              'public/components/backbone.marionette/public/javascripts/json2.js',
-              'public/components/backbone.marionette/public/javascripts/underscore.js',
-              'public/components/backbone.marionette/public/javascripts/backbone.js',
-              'public/components/backbone.marionette/lib/backbone.marionette.min.js'
-            ],
-            dest : '<%= js_deps %>.js'
-          }
-        ]
-      },
-      all : {
-        files : [
-          {
-            src : [
-              '<%= js_deps %>.min.js',
-              'public/components/requirejs/require.js', /** todo - Automate the insertion of this file path **/
-              '<%= js_amd %>.js'
-            ],
-            dest : '<%= js_all %>.js'
-          }
-        ]
-      }
-    },
-    uglify : {
-      deps : {
-        files : [
-          {
-            src  : '<%= js_deps %>.js',
-            dest : '<%= js_deps %>.min.js'
-          }
-        ]
-      },
-      all : {
-        files : [
-          {
-            src  : '<%= js_all %>.js',
-            dest : '<%= js_all %>.min.js'
-          }
-        ]
-      }
-    },
-    compass : {
-      dist : {
-        options : {
-          sassDir     : '<%= sass_dir %>',
-          cssDir      : '<%= css_dir %>',
-          environment : 'production',
-          outputStyle : 'compressed'
-        }
-      },
-      dev  : {
-        options : {
-          sassDir : '<%= sass_dir %>',
-          cssDir  : '<%= css_dir %>',
-          outputStyle : 'expanded'
-        }
-      }
-    },
-    watch : {
-      compass : {
-        files : '<%= sass_dir %>/**',
-        tasks : ['compass', 'livereload']
-      }
-    },
-    requirejs : {
-      compile : {
-        options : {
-          baseUrl : 'public/js/src',
-          name    : 'main',
-          out     : "<%= js_amd %>.js"
-        }
-      }
-    },
-    regarde : {
-      css : {
-        files : '<%= sass_dir %>/**',
-        tasks : ['compass', 'livereload']
-      }
-    },
-    removelogging : {
-      dist : {
-        src  : "<%= js_all %>.min.js",
-        dest : "<%= js_dist_file %>"
-      }
-    },
-    karma : {
-      unit : {
-        configFile : 'public/js/karma.conf.js'
-      }
-    },
-    cachebreaker : {
-      js : {
-        options : {
-          asset_url : '<%= js_dist_file %>',
-          remove    : 'public'
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            js: 'public/js',
+            css: 'public/css',
+            sass: 'public/sass',
+            bower: 'public/components',
+            php: {
+                layout: 'app/views/layouts'
+            }
         },
-        files   : {
-          '<%= php_files.layout %>' : ['<%= php_files.layout %>']
-        }
-      },
-      css : {
-        options : {
-          asset_url : '<%= css_dist_file %>',
-          remove    : 'public'
+        files: {
+            js: {
+                js_deps: '<%= dirs.js %>/dist/deps',
+                js_amd: '<%= dirs.js %>/dist/modules',
+                js_all: '<%= dirs.js %>/dist/combined',
+                js_dist_file: '<%= files.js.js_all %>.min.js'
+            },
+            css: {
+                dist: '<%= dirs.css %>/style.css'
+
+            },
+            php: {
+                layout: '<%= dirs.php.layout %>/master.blade.php'
+            }
         },
-        files   : {
-          '<%= php_files.layout %>' : ['<%= php_files.layout %>']
+        concat: {
+            deps: {
+                files: [
+                    {
+                        src: [
+                            '<%= dirs.bower %>/jquery/jquery.min.js',
+                            '<%= dirs.bower %>/backbone.marionette/public/javascripts/json2.js',
+                            '<%= dirs.bower %>/backbone.marionette/public/javascripts/underscore.js',
+                            '<%= dirs.bower %>/backbone.marionette/public/javascripts/backbone.js',
+                            '<%= dirs.bower %>/backbone.marionette/lib/backbone.marionette.min.js'
+                        ],
+                        dest: '<%= files.js.js_deps %>.js'
+                    }
+                ]
+            },
+            all: {
+                files: [
+                    {
+                        src: [
+                            '<%= files.js.js_deps %>.min.js',
+                            '<%= dirs.bower %>/requirejs/require.js',
+                            '<%= files.js.js_amd %>.js'
+                        ],
+                        dest: '<%= files.js.js_all %>.js'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            deps: {
+                files: [
+                    {
+                        src: '<%= files.js.js_deps %>.js',
+                        dest: '<%= files.js.js_deps %>.min.js'
+                    }
+                ]
+            },
+            all: {
+                files: [
+                    {
+                        src: '<%= files.js.js_all %>.js',
+                        dest: '<%= files.js.js_all %>.min.js'
+                    }
+                ]
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir: '<%= dirs.sass %>',
+                    cssDir: '<%= dirs.css %>',
+                    environment: 'production',
+                    outputStyle: 'compressed'
+                }
+            },
+            dev: {
+                options: {
+                    sassDir: '<%= dirs.sass %>',
+                    cssDir: '<%= dirs.css %>',
+                    outputStyle: 'expanded'
+                }
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: '<%= dirs.js %>/src',
+                    name: 'main',
+                    out: "<%= files.js.js_amd %>.js"
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '<%= dirs.sass %>/**',
+                tasks: ['compass']
+            },
+            jshint: {
+                files: ['<%= dirs.js %>/src/**/*.js', '<%= dirs.js %>/test/**/*.js'],
+                tasks: 'jshint'
+            }
+        },
+        browser_sync: {
+            files: {
+                src: '<%= dirs.css %>/style.css'
+            },
+            options: {
+                watchTask: true
+            }
+        },
+        karma: {
+            unit: {
+                configFile: '<%= dirs.js %>/karma.conf.js',
+                singleRun: true
+            },
+            watch: {
+                configFile: '<%= dirs.js %>/karma.conf.js',
+                singleRun: false
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '<%= dirs.js %>/.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib: {
+                src: ['<%= dirs.js %>/src/**/*.js']
+            },
+            test: {
+                src: ['<%= dirs.js %>/test/**/*.js']
+            }
+        },
+        cachebreaker: {
+            js: {
+                options: {
+                    asset_url: '<%= files.js.js_dist_file %>',
+                    remove: 'public'
+                },
+                files: {
+                    '<%= files.php.layout %>': ['<%= files.php.layout %>']
+                }
+            },
+            css: {
+                options: {
+                    asset_url: '<%= files.css.dist %>',
+                    remove: 'public'
+                },
+                files: {
+                    '<%= files.php.layout %>': ['<%= files.php.layout %>']
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-  grunt.loadNpmTasks( 'grunt-contrib-watch' );
-  grunt.loadNpmTasks( 'grunt-contrib-concat' );
-  grunt.loadNpmTasks( 'grunt-contrib-compass' );
-  grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
-  grunt.loadNpmTasks( 'grunt-regarde' );
-  grunt.loadNpmTasks( 'grunt-contrib-livereload' );
-  grunt.loadNpmTasks( "grunt-remove-logging" );
-  grunt.loadNpmTasks( 'grunt-karma' );
-  grunt.loadNpmTasks( 'grunt-cache-breaker' );
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-livereload');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-cache-breaker');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  /** Build Javascript Dependencies into single file **/
-  grunt.registerTask( 'js-deps', ['concat:deps', 'uglify:deps'] );
+    /** Build Javascript Dependencies into single file **/
+    grunt.registerTask('js-deps', ['concat:deps', 'uglify:deps']);
 
-  /** Build the personal AMD modules into a single file **/
-  grunt.registerTask( 'js-amd', ['requirejs', 'concat:all'] );
+    /** Build the personal AMD modules into a single file **/
+    grunt.registerTask('js-amd', ['requirejs', 'concat:all']);
 
-  /** Combine Dependencies, RequireJS & all Modules into a single file. **/
-  grunt.registerTask( 'js-all', ['concat:deps', 'uglify:deps', 'requirejs', 'concat:all', 'uglify:all', 'removelogging', 'cachebreaker:js'] );
+    /** Lint & run tests */
+    grunt.registerTask('test', ['jshint', 'concat:deps', 'karma:unit']);
 
-  /** Run the Live reload server (watches files in the public/sass directory **/
-  grunt.registerTask( 'server-sass', ['livereload-start', 'regarde:css']);
+    /** Watch & run tests on file-change */
+    grunt.registerTask('test-watch', ['concat:deps', 'jshint', 'karma:watch']);
+
+    /** Combine Dependencies, RequireJS & all Modules into a single file. **/
+    grunt.registerTask('build-js', ['jshint', 'concat:deps', 'uglify:deps', 'requirejs', 'concat:all', 'uglify:all', 'cachebreaker:js']);
+
+    /** Run compass & the CSS cachebreaker **/
+    grunt.registerTask('build-css', ['compass', 'cachebreaker:css']);
+
+    /** Run JS tests, build JS and build CSS */
+    grunt.registerTask('release', ['test', 'build-js', 'build-css']);
+
+    /** Run the Live reload server (watches files in the public/sass directory **/
+    grunt.registerTask('server-sass', ['browser_sync', 'watch']);
 
 };
