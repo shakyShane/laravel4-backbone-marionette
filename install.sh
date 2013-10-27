764561c913793968a108ef136aa077b09ef7e91f
Shane Osbourne <shane.osbourne8@gmail.com>
2013-10-27 15:15:10 +0000

    add script

---
+++ install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/shakyShane/laravel4-backbone-marionette.git $1
+cd $1
+rm -rf .git
+curl -Ss http://getcomposer.org/installer | php
+php composer.phar install
+npm install
+bower install
+php artisan key:generate
+php artisan serve
